320970db889c8d3666e4abda9f5bef0278d280d1
shobhitagg <shobhita@andrew.cmu.edu>
2024-01-29 15:03:58 -0500

    change name

--- doc/conf.py
+++ doc/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'MFI Hazelbots Lab'
+project = 'MFI Hazelbots Testbed'
 copyright = 'CMU'
 author = 'Manufacturing Futures Institute'
 release = '1.0'
